a5de4817247000c13f2fa2b892acdb8866e1c684
NguyenLeChiDai <chidai2411202@gmail.com>
2024-12-05 09:41:22 +0700

    update deploy3

--- server/index.js
+++ server/index.js
@@ -89,7 +89,7 @@ app.use("/api/teachersManagement", teachersManagement);
 //------- Deploy
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../frontend/build")));
+  app.use(express.static(path.join(__dirname1, "../client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname1, "../client", "build", "index.html"));
   });
